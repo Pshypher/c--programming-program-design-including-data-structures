e11d5f9028513e8d631c0ce51b55c86b42d3a7bb
Jimi Shote@fyre <babajimi.shote@fairmoney.io>
2026-03-17 06:37:05 +0100

    Adds Programming Exercises 6-31 through 6-33 and updates 6-30
    - Implements Programming Exercise 6-31 to calculate the length of a pipe based on hallway dimensions.
    - Implements Programming Exercise 6-32 to compute the greatest common divisor of two integers.
    - Implements Programming Exercise 6-33 to factorize a second-degree polynomial.
    - Updates Programming Exercise 6-30 to output distances in miles and renames `ONE_STEP_IN_FOOT` to `ONE_FOOT`.
    - Updates CMakeLists.txt to build Programming Exercise 6-33.

--- Ch6/Programming_Exercises/Programming_Exercise6_30.cpp
+++ Ch6/Programming_Exercises/Programming_Exercise6_30.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 const double FOOT_PER_MILE = 5280.0;
-const double ONE_STEP_IN_FOOT = 1;
+const double ONE_FOOT = 1;
 
 double getDistance(double& land, double& water);
 double getCost(double& land, double& water);
@@ -30,8 +30,8 @@ int main()
     cout << fixed << showpoint << setprecision(2);
 
     double minimum = minimumCost(x, y, cableCostLand, cableCostWater, minimumLengthLand, minimumLengthWater);
-    cout << minimumLengthWater << " feet is run under water." << endl;
-    cout << minimumLengthLand << " feet is run downstream to the factory over land." << endl;
+    cout << minimumLengthWater / FOOT_PER_MILE << " miles is run under water." << endl;
+    cout << minimumLengthLand / FOOT_PER_MILE << " miles is run downstream to the factory over land." << endl;
     cout << "The total minimum cost is $" << minimum << endl;
 
     return 0;
@@ -67,7 +67,7 @@ double minimumCost(double x, double y, double cableCostLand, double cableCostWat
     double x1 = 0;
     while (x > 0)
     {
-        x1 = x1 + ONE_STEP_IN_FOOT;
+        x1 = x1 + ONE_FOOT;
         double z = sqrt(pow(y, 2) + pow(x - x1, 2));
         double cost = cableCostLand * x1 + cableCostWater * z;
 
@@ -78,7 +78,7 @@ double minimumCost(double x, double y, double cableCostLand, double cableCostWat
             minimumWater = z;
         }
 
-        x = x - ONE_STEP_IN_FOOT;
+        x = x - ONE_FOOT;
     }
 
     return minimum;
